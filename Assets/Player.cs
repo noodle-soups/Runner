40bfcfe3ee645f04d467a1135b2b59d77c9765e6
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2024-08-05 20:31:05 +0100

    commit

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,70 +11,58 @@ public class Player : MonoBehaviour
     [SerializeField] private Rigidbody2D _rb;
 
     [Header("Movement")]
-    private Vector2 _moveDir = Vector2.zero;
+    private float _xInput;
     [SerializeField] private float _moveSpeed;
 
     [Header("Jump")]
     [SerializeField] private float _jumpForce;
 
-    [Header("Input Actions")]
-    [SerializeField] private PlayerInputActions _inputActions;
-    private InputAction _move;
-    private InputAction _fire;
-    private InputAction _jump;
+    [Header("Ground Check")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    protected private bool isGrounded;
 
-    private void Awake()
-    {
-        _inputActions = new PlayerInputActions();
-    }
+    //[Header("Direction")]
+    //private bool isFacingRight = true;
 
-    private void OnEnable()
-    {
-        // move
-        _move = _inputActions.Player.Move;
-        _move.Enable();
-
-        // fire
-        _fire = _inputActions.Player.Fire;
-        _fire.Enable();
-        _fire.performed += Fire;
-
-        // jump
-        _jump = _inputActions.Player.Jump;
-        _jump.Enable();
-        _jump.performed += Jump;
-
-    }
-
-    private void OnDisable()
-    {
-        _move.Disable();
-        _fire.Disable();
-    }
 
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
     }
 
-
     void Update()
     {
-        _moveDir = _move.ReadValue<Vector2>();
+        GroundCheck();
     }
 
     private void FixedUpdate()
     {
-        _rb.velocity = new Vector2(_moveDir.x * _moveSpeed, _rb.velocity.y);
+        _rb.velocity = new Vector2(_xInput * _moveSpeed, _rb.velocity.y);
     }
 
-    private void Fire(InputAction.CallbackContext context)
+    public void Move(InputAction.CallbackContext context)
     {
-        Debug.Log("FIRE!!!");
+        _xInput = context.ReadValue<Vector2>().x;
     }
 
-    private void Jump(InputAction.CallbackContext context)
+    public void Jump(InputAction.CallbackContext context)
     {
-        Debug.Log("JUMP!!!");
+        if (context.performed && isGrounded)
+            _rb.velocity = new Vector2(_rb.velocity.x, _jumpForce);
     }
+
+    // ground check
+    private void GroundCheck()
+    {
+        isGrounded = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+
+    // gizmos
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
+    }
+
 }
