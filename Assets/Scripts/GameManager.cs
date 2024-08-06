11bf977a4e1977080d3a7e3f71e24105eeded96e
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2024-08-06 18:46:10 +0100

    commit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,13 +5,33 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+
+    [Header("Player Score")]
     [SerializeField] private int _playerScore = 0;
     [SerializeField] private Text _scoreText;
+
+    [Header("Time")]
+    [SerializeField] private float _timeInSeconds;
+    [SerializeField] private Text _timeText;
+
+    private void Update()
+    {
+        DisplayTime();
+    }
 
     public void AddScore(int scoreToAdd)
     {
         _playerScore += scoreToAdd;
         _scoreText.text = "Score: " + _playerScore.ToString();
+    }
+
+    private void DisplayTime()
+    {
+        _timeInSeconds += Time.deltaTime;
+        int _mm = Mathf.FloorToInt(_timeInSeconds / 60f);
+        int _ss = Mathf.FloorToInt(_timeInSeconds % 60f);
+        int _ms = Mathf.FloorToInt((_timeInSeconds * 1000) % 1000f);
+        _timeText.text = "Time: " + string.Format("{0:00}:{1:00}:{2:000}", _mm, _ss, _ms);
     }
 
 }
